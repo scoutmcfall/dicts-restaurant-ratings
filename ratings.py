6b3e8d44555bb0021670f9e9841ad53e8a4594f9
Scout McFall <scoutmknz@gmail.com>
2021-09-07 23:18:42 +0000

    alphabetizes now

--- ratings.py
+++ ratings.py
@@ -6,12 +6,13 @@ rate_file = open("scores.txt")
 #Prompt the user for a restaurant name
 r_name = input("What's the restaurant name?")
 #Prompt the user for a restaurant score
-score = input("What's the restaurant score?")
+score = int(input("What's the restaurant score?"))
 
 #Store the new restaurant/rating in the dictionary
 rest_dict = {}
-rest_dict[r_name] = score
+
 ratings = []
+ratings.append([r_name, score])
 #split each line up by ":" and put them in a list called ratings
 for line in rate_file:
     line = line.rstrip()
@@ -26,9 +27,7 @@ for elem in ratings_2:
 
 #And finally, spits out the ratings in alphabetical order by restaurant
 #Print all of the ratings in alphabetical order (including the new one, of course)
-#print(rest_dict)
-#for key, value in rest_dict:
-    #print (f"{key} is rated at {value}")
+
 for key, value in rest_dict.items():
-    print(f"{key} is rated at {value}")
+    print(f"{key} is rated {value}")
 #return None
