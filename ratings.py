f519dba89b3c9368a0f45e03805bacb450edea0d
Scout McFall <scoutmknz@gmail.com>
2021-09-07 23:11:47 +0000

    make vertical aphabetized dictionary, not done yet

--- ratings.py
+++ ratings.py
@@ -1,12 +1,34 @@
 """Restaurant rating lister."""
 
 #Reads the ratings in from the file
+rate_file = open("scores.txt")
+
+#Prompt the user for a restaurant name
+r_name = input("What's the restaurant name?")
+#Prompt the user for a restaurant score
+score = input("What's the restaurant score?")
+
+#Store the new restaurant/rating in the dictionary
+rest_dict = {}
+rest_dict[r_name] = score
+ratings = []
+#split each line up by ":" and put them in a list called ratings
+for line in rate_file:
+    line = line.rstrip()
+    strip_line = line.split(":")
+    ratings.append(strip_line)
+#sorts them using sorted function
+ratings_2 = sorted(ratings)
 
 #Stores them in a dictionary
-#.items() returns a list of dictionary entries
-#sorts them using sorted function
+for elem in ratings_2:
+    rest_dict[elem[0]] = elem[1]
 
 #And finally, spits out the ratings in alphabetical order by restaurant
-#for key, value in dictionary:
-# print (key "is rated at" value)
-
+#Print all of the ratings in alphabetical order (including the new one, of course)
+#print(rest_dict)
+#for key, value in rest_dict:
+    #print (f"{key} is rated at {value}")
+for key, value in rest_dict.items():
+    print(f"{key} is rated at {value}")
+#return None
