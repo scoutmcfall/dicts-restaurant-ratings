333bd5fb19d557cd974573acc2f5fedb9e06f996
Scout McFall <scoutmknz@gmail.com>
2021-09-07 21:48:37 +0000

    instructions

--- ratings.py
+++ ratings.py
@@ -1,4 +1,11 @@
 """Restaurant rating lister."""
 
+#Reads the ratings in from the file
 
-# put your code here
+#Stores them in a dictionary
+#.items() returns a list of dictionary entries
+#sorts them using sorted function
+
+#And finally, spits out the ratings in alphabetical order by restaurant
+#print (key "is rated at" value)
+
